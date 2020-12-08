cea4733ba487212cf1d02273958ddc23e506ad25
Huat Zhi <huatzhi@gmail.com>
2020-12-08 10:33:46 +0800

    init first commit

--- cdk_test/cdk_test_stack.py
+++ cdk_test/cdk_test_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+)
 
 
 class CdkTestStack(core.Stack):
@@ -7,3 +10,11 @@ class CdkTestStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="test-cdk-huat-4e8f4f8",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
---
+++ questions.md
@@ -0,0 +1,5 @@
+# Questions
+
+- What does a stack do? What actually is a stack in aws?
+- cdk ls?
+- cdk synth?
